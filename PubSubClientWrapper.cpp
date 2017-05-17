8a11e13d451eb905145b2f31b03e359f28ec093a
Dieter Niklaus <dieter.niklaus@gmx.net>
2017-05-17 23:28:09 +0200

    messageReceived(): remove useless "\n"

--- PubSubClientWrapper.cpp
+++ PubSubClientWrapper.cpp
@@ -141,7 +141,7 @@ void PubSubClientCallbackAdapter::messageReceived(char* topic, unsigned char* pa
   memcpy(msg, payload, length);
   msg[length] = 0;
 
-  TR_PRINTF(m_trPortMqttRx, DbgTrace_Level::info, "Message arrived, topic: %s - msg: %s\n", topic, msg);
+  TR_PRINTF(m_trPortMqttRx, DbgTrace_Level::info, "Message arrived, topic: %s - msg: %s", topic, msg);
 
   m_rxMsg->prepare(topic, payload, length);
 
