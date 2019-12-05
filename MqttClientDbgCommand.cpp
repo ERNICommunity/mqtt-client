de0619538802eb5981ff735f3c9ac3ad92986c00
Dieter Niklaus <dieter.niklaus@gmx.net>
2019-12-05 11:57:45 +0100

    new: enable ESP32 chg: replace Serial.printUsage() by DbgTrace TR_PRINTF()

--- MqttClientDbgCommand.cpp
+++ MqttClientDbgCommand.cpp
@@ -37,15 +37,15 @@ void DbgCli_Cmd_MqttClientCon::execute(unsigned int argc, const char** args, uns
     if (0 != m_mqttClient)
     {
       m_mqttClient->setShallConnect(true);
-      Serial.println("MQTT client connecting to broker now.");
+      TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "MQTT client connecting to broker now.");
     }
   }
 }
 
 void DbgCli_Cmd_MqttClientCon::printUsage()
 {
-  Serial.println(getHelpText());
-  Serial.println("Usage: dbg mqtt con");
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "%s", getHelpText());
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "Usage: %s %s %s", RootNode()->getNodeName(), getParentNode()->getNodeName(), getNodeName());  
 }
 
 //-----------------------------------------------------------------------------
@@ -66,15 +66,15 @@ void DbgCli_Cmd_MqttClientDis::execute(unsigned int argc, const char** args, uns
     if (0 != m_mqttClient)
     {
       m_mqttClient->setShallConnect(false);
-      Serial.println("MQTT client disconnecting from broker now.");
+      TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "MQTT client disconnecting from broker now.");
     }
   }
 }
 
 void DbgCli_Cmd_MqttClientDis::printUsage()
 {
-  Serial.println(getHelpText());
-  Serial.println("Usage: dbg mqtt dis");
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "%s", getHelpText());
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "Usage: %s %s %s", RootNode()->getNodeName(), getParentNode()->getNodeName(), getNodeName());  
 }
 
 //-----------------------------------------------------------------------------
@@ -97,19 +97,22 @@ void DbgCli_Cmd_MqttClientPub::execute(unsigned int argc, const char** args, uns
       MqttTopicPublisher* publisher = m_mqttClient->findPublisherByTopic(args[idxToFirstArgToHandle]);
       if (0 == publisher)
       {
-        publisher = new MqttTopicPublisher(args[idxToFirstArgToHandle], args[idxToFirstArgToHandle+1]);
+        publisher = new MqttTopicPublisher(args[idxToFirstArgToHandle], args[idxToFirstArgToHandle+1], MqttTopicPublisher::DO_AUTO_PUBLISH);
       }
-      publisher->publish();
-      Serial.print("MQTT client, published to ");
-      Serial.println(args[idxToFirstArgToHandle+1]);
+	  else
+      {
+        publisher->setData(args[idxToFirstArgToHandle+1]);
+        publisher->publish();
+      }
+      TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "MQTT client, published to %s",  publisher->getTopicString(), publisher->getData());
     }
   }
 }
 
 void DbgCli_Cmd_MqttClientPub::printUsage()
 {
-  Serial.println(getHelpText());
-  Serial.println("Usage: dbg mqtt pub <topic> <value>");
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "%s", getHelpText());
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "Usage: %s %s %s <topic> <value>", RootNode()->getNodeName(), getParentNode()->getNodeName(), getNodeName());  
 }
 
 //-----------------------------------------------------------------------------
@@ -132,14 +135,12 @@ void DbgCli_Cmd_MqttClientSub::execute(unsigned int argc, const char** args, uns
       MqttTopicSubscriber* subscriber = m_mqttClient->findSubscriberByTopic(args[idxToFirstArgToHandle]);
       if (0 == subscriber)
       {
-        new DefaultMqttSubscriber(args[idxToFirstArgToHandle]);
-        Serial.print("MQTT client, subscribed to ");
-        Serial.println(args[idxToFirstArgToHandle]);
+        subscriber = new DefaultMqttSubscriber(args[idxToFirstArgToHandle]);
+        TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "MQTT client, subscribed to %s", subscriber->getTopicString());
       }
       else
       {
-        Serial.print("MQTT client, already subscribed to ");
-        Serial.println(args[idxToFirstArgToHandle]);
+        TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "MQTT client, already subscribed to %s", subscriber->getTopicString());
       }
     }
   }
@@ -147,8 +148,8 @@ void DbgCli_Cmd_MqttClientSub::execute(unsigned int argc, const char** args, uns
 
 void DbgCli_Cmd_MqttClientSub::printUsage()
 {
-  Serial.println(getHelpText());
-  Serial.println("Usage: dbg mqtt sub <topic>");
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "%s", getHelpText());
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "Usage: %s %s %s <topic>", RootNode()->getNodeName(), getParentNode()->getNodeName(), getNodeName());  
 }
 
 //-----------------------------------------------------------------------------
@@ -171,14 +172,13 @@ void DbgCli_Cmd_MqttClientUnsub::execute(unsigned int argc, const char** args, u
       MqttTopicSubscriber* subscriber = m_mqttClient->findSubscriberByTopic(args[idxToFirstArgToHandle]);
       if (0 != subscriber)
       {
+        TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "MQTT client, unsubscribing from %s", subscriber->getTopicString());
         delete subscriber;
-        Serial.print("MQTT client, unsubscribed from ");
-        Serial.println(args[idxToFirstArgToHandle]);
+        subscriber = 0;
       }
       else
       {
-        Serial.print("MQTT client, unsubscribe not possible, topic was not subscribed: ");
-        Serial.println(args[idxToFirstArgToHandle]);
+        TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "MQTT client, unsubscribe not possible, topic %s was not subscribed.", args[idxToFirstArgToHandle]);
       }
     }
   }
@@ -186,8 +186,8 @@ void DbgCli_Cmd_MqttClientUnsub::execute(unsigned int argc, const char** args, u
 
 void DbgCli_Cmd_MqttClientUnsub::printUsage()
 {
-  Serial.println(getHelpText());
-  Serial.println("Usage: dbg mqtt unsub <topic>");
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "%s", getHelpText());
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "Usage: %s %s %s <topic>", RootNode()->getNodeName(), getParentNode()->getNodeName(), getNodeName());  
 }
 
 //-----------------------------------------------------------------------------
@@ -199,33 +199,30 @@ DbgCli_Cmd_MqttClientShow::DbgCli_Cmd_MqttClientShow(DbgCli_Topic* mqttClientTop
 
 void DbgCli_Cmd_MqttClientShow::execute(unsigned int argc, const char** args, unsigned int idxToFirstArgToHandle)
 {
-  Serial.println("Subscriber Topics:");
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "Subscriber Topics:");
   MqttTopicSubscriber* subscriber = m_mqttClient->mqttSubscriberChain();
   if (0 == subscriber)
   {
-    Serial.println("no subscribers in the list.");
+    TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "=x= no subscribers in the list.");
   }
   while (0 != subscriber)
   {
-    Serial.println(subscriber->getTopicString());
+    TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "- %s", subscriber->getTopicString());
     subscriber = subscriber->next();
   }
-  Serial.println("Publisher Topics:");
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "Publisher Topics:");
   MqttTopicPublisher* publisher = m_mqttClient->mqttPublisherChain();
   if (0 == publisher)
   {
-    Serial.println("no publishers in the list.");
+    TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "=x= no publishers in the list.");
   }
   while (0 != publisher)
   {
-    Serial.println(publisher->getTopicString());
+    TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "- %s", publisher->getTopicString());
     publisher = publisher->next();
   }
-  Serial.print("MQTT Client Status: Shall ");
-  Serial.print(m_mqttClient->getShallConnect() ? "" : "not ");
-  Serial.println("connect");
-  Serial.print("Connection Monitor State: ");
-  Serial.println(m_mqttClient->connMon()->state()->toString());
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "MQTT Client Status: Shall %s connect", m_mqttClient->getShallConnect() ? "" : "not ");
+  TR_PRINTF(m_mqttClient->trPort(), DbgTrace_Level::alert, "Connection Monitor State: %s", m_mqttClient->connMon()->state()->toString());
   m_mqttClient->loop();
 }
 
